3836fcad3fbd96f7b8eb10f8f7897349799273d6
K <kayo@illumium.org>
2020-02-10 19:35:58 +0500

    Updated samples writing interface

--- fluidlite/src/synth/write.rs
+++ fluidlite/src/synth/write.rs
@@ -2,54 +2,42 @@ use crate::{ffi, Synth, Status};
 
 /// The trait which implements samples data buffer interface
 pub trait IsSamples {
-    fn write_samples(self, handle: *mut ffi::fluid_synth_t) -> i32;
+    fn write_samples(self, synth: &Synth) -> Status;
 }
 
 impl IsSamples for &mut [i16] {
     /// Write samples interleaved
-    fn write_samples(self, handle: *mut ffi::fluid_synth_t) -> i32 {
+    fn write_samples(self, synth: &Synth) -> Status {
         let len = self.len() / 2;
-        unsafe {
-            ffi::fluid_synth_write_s16(handle, len as _,
-				                               self.as_mut_ptr() as _, 0, 2,
-				                               self.as_mut_ptr() as _, 1, 2)
-        }
+        unsafe { synth.write_i16(
+            len, self.as_mut_ptr(), 0, 2, self.as_mut_ptr(), 1, 2) }
     }
 }
 
 impl IsSamples for (&mut [i16], &mut [i16]) {
     /// Write samples non-interleaved
-    fn write_samples(self, handle: *mut ffi::fluid_synth_t) -> i32 {
+    fn write_samples(self, synth: &Synth) -> Status {
         let len = self.0.len().min(self.1.len());
-        unsafe {
-            ffi::fluid_synth_write_s16(handle, len as _,
-				                               self.0.as_mut_ptr() as _, 0, 1,
-				                               self.1.as_mut_ptr() as _, 0, 1)
-        }
+        unsafe { synth.write_i16(
+            len, self.0.as_mut_ptr(), 0, 1, self.1.as_mut_ptr(), 0, 1) }
     }
 }
 
 impl IsSamples for &mut [f32] {
     /// Write samples interleaved
-    fn write_samples(self, handle: *mut ffi::fluid_synth_t) -> i32 {
+    fn write_samples(self, synth: &Synth) -> Status {
         let len = self.len() / 2;
-        unsafe {
-            ffi::fluid_synth_write_float(handle, len as _,
-				                                 self.as_mut_ptr() as _, 0, 2,
-				                                 self.as_mut_ptr() as _, 1, 2)
-        }
+        unsafe { synth.write_f32(
+            len, self.as_mut_ptr(), 0, 2, self.as_mut_ptr(), 1, 2) }
     }
 }
 
 impl IsSamples for (&mut [f32], &mut [f32]) {
     /// Write samples non-interleaved
-    fn write_samples(self, handle: *mut ffi::fluid_synth_t) -> i32 {
+    fn write_samples(self, synth: &Synth) -> Status {
         let len = self.0.len().min(self.1.len());
-        unsafe {
-            ffi::fluid_synth_write_float(handle, len as _,
-				                                 self.0.as_mut_ptr() as _, 0, 1,
-				                                 self.1.as_mut_ptr() as _, 0, 1)
-        }
+        unsafe { synth.write_f32(
+            len, self.0.as_mut_ptr(), 0, 1, self.1.as_mut_ptr(), 0, 1) }
     }
 }
 
@@ -58,9 +46,41 @@ Synthesizer plugin
  */
 impl Synth {
     /**
-    Write sound samples to sample data buffer
+    Write sound samples to the sample data buffer
      */
     pub fn write<S: IsSamples>(&self, samples: S) -> Status {
-        self.zero_ok(samples.write_samples(self.handle))
+        samples.write_samples(self)
+    }
+
+    /**
+    Write samples as 16-bit signed integers
+
+    __Note__: The `len` must corresponds to the lenghtes of buffers.
+     */
+    #[inline]
+    pub unsafe fn write_i16(&self,
+                            len: usize,
+                            lbuf: *mut i16, loff: u32, lincr: u32,
+                            rbuf: *mut i16, roff: u32, rincr: u32) -> Status {
+        self.zero_ok(ffi::fluid_synth_write_s16(
+            self.handle, len as _,
+				    lbuf as _, loff as _, lincr as _,
+				    rbuf as _, roff as _, rincr as _))
+    }
+
+    /**
+    Write samples as 32-bit floating-point numbers
+
+    __Note__: The `len` must corresponds to the lenghtes of buffers.
+     */
+    #[inline]
+    pub unsafe fn write_f32(&self,
+                            len: usize,
+                            lbuf: *mut f32, loff: u32, lincr: u32,
+                            rbuf: *mut f32, roff: u32, rincr: u32) -> Status {
+        self.zero_ok(ffi::fluid_synth_write_float(
+            self.handle, len as _,
+				    lbuf as _, loff as _, lincr as _,
+				    rbuf as _, roff as _, rincr as _))
     }
 }
