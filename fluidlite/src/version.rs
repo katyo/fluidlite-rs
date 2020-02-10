876534642d75d8e23d7e9e9f231e4f72b6fe7241
K <kayo@illumium.org>
2020-02-10 19:08:11 +0500

    Fixed lib version info getting

--- fluidlite/src/version.rs
+++ fluidlite/src/version.rs
@@ -1,6 +1,12 @@
-use std::mem::MaybeUninit;
+use std::{
+    mem::MaybeUninit,
+    fmt::{Display, Formatter, Result as FmtResult},
+};
 use crate::ffi;
 
+/**
+The library version info
+ */
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Version {
     pub major: u32,
@@ -8,20 +14,49 @@ pub struct Version {
     pub micro: u32,
 }
 
-pub fn version() -> Version {
-    let mut major = MaybeUninit::uninit();
-    let mut minor = MaybeUninit::uninit();
-    let mut micro = MaybeUninit::uninit();
+impl Display for Version {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        self.major.fmt(f)?;
+        '.'.fmt(f)?;
+        self.minor.fmt(f)?;
+        '.'.fmt(f)?;
+        self.micro.fmt(f)
+    }
+}
 
-    unsafe { ffi::fluid_version(
-        major.as_mut_ptr(),
-        minor.as_mut_ptr(),
-        micro.as_mut_ptr(),
-    ); }
+impl Version {
+    pub fn new(major: u32, minor: u32, micro: u32) -> Self {
+        Self { major, minor, micro }
+    }
 
-    Version {
-        major: unsafe { major.assume_init() as _ },
-        minor: unsafe { minor.assume_init() as _ },
-        micro: unsafe { micro.assume_init() as _ },
+    pub fn get() -> Version {
+        let mut major = MaybeUninit::uninit();
+        let mut minor = MaybeUninit::uninit();
+        let mut micro = MaybeUninit::uninit();
+
+        unsafe { ffi::fluid_version(
+            major.as_mut_ptr(),
+            minor.as_mut_ptr(),
+            micro.as_mut_ptr(),
+        ); }
+
+        Version::new(
+            unsafe { major.assume_init() as _ },
+            unsafe { minor.assume_init() as _ },
+            unsafe { micro.assume_init() as _ },
+        )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Version;
+
+    #[test]
+    fn version() {
+        let ver = Version::get();
+
+        assert_eq!(ver, Version::new(1, 2, 0));
+        assert_eq!(ver.to_string(), "1.2.0");
     }
 }
