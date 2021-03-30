c8ee51cb61ed91400100e5360cc666254c3d598b
K <kayo@illumium.org>
2021-03-30 15:40:39 +0500

    Switch library build rules from cmake in favor of cc

--- fluidlite-lib/build.rs
+++ fluidlite-lib/build.rs
@@ -15,16 +15,11 @@ fn main() {
         );
 
         let out_dir = env::var("OUT_DIR").expect("The OUT_DIR is set by cargo.");
-
         let out_dir = Path::new(&out_dir);
-
         let src_dir = out_dir.join("source").join(&src.version);
 
-        let bld_dir = out_dir.join("build").join(&src.version);
-
         utils::fetch_source(&src, &src_dir);
-
-        utils::compile_library(&src_dir, &bld_dir);
+        utils::compile_library(&src_dir, &out_dir);
     }
 }
 
@@ -85,46 +80,152 @@ mod utils {
         }
     }
 
-    pub fn bool_flag(flag: bool) -> &'static str {
-        if flag {
-            "ON"
-        } else {
-            "OFF"
-        }
+    fn add_lib(name: &str, static_: bool) {
+        println!(
+            "cargo:rustc-link-lib={}{}",
+            if static_ { "static=" } else { "" },
+            name
+        );
     }
 
-    pub fn compile_library(src_dir: &Path, out_dir: &Path) {
-        use cmake::Config;
-
-        let lib_dir = out_dir.join("lib");
-
+    pub fn compile_library(src_dir: &Path, lib_dir: &Path) {
         let lib_name = "fluidlite";
 
         if !lib_dir
             .join(lib_file(&lib_name, cfg!(feature = "shared")))
             .is_file()
         {
-            std::fs::create_dir_all(out_dir).unwrap();
+            let mut build = cc::Build::new();
 
-            Config::new(src_dir)
-                //.define("FLUIDLITE_BUILD_SHARED", bool_flag(cfg!(feature = "shared")))
-                //.define("FLUIDLITE_BUILD_STATIC", bool_flag(!cfg!(feature = "shared")))
-                .define("ENABLE_SF3", bool_flag(cfg!(feature = "with-sf3")))
-                .define("STB_VORBIS", bool_flag(cfg!(feature = "with-stb")))
-                .define("CMAKE_C_COMPILER_WORKS", bool_flag(true))
-                .define("CMAKE_CXX_COMPILER_WORKS", bool_flag(true))
-                .always_configure(true)
-                .very_verbose(true)
-                .out_dir(out_dir)
-                .build();
+            build.flag_if_supported("-std=c99");
+
+            build.include(src_dir.join("include"));
+            build.files(
+                [
+                    "fluid_init.c",
+                    "fluid_chan.c",
+                    "fluid_chorus.c",
+                    "fluid_conv.c",
+                    "fluid_defsfont.c",
+                    "fluid_dsp_float.c",
+                    "fluid_gen.c",
+                    "fluid_hash.c",
+                    "fluid_list.c",
+                    "fluid_mod.c",
+                    "fluid_ramsfont.c",
+                    "fluid_rev.c",
+                    "fluid_settings.c",
+                    "fluid_synth.c",
+                    "fluid_sys.c",
+                    "fluid_tuning.c",
+                    "fluid_voice.c",
+                ]
+                .iter()
+                .map(|src| src_dir.join("src").join(src)),
+            );
+
+            #[cfg(feature = "with-sf3")]
+            {
+                #[cfg(feature = "with-stb")]
+                {
+                    build.define("SF3_SUPPORT", "SF3_STB_VORBIS");
+                    build.include(src_dir.join("stb"));
+                    build.file(src_dir.join("stb").join("stb_vorbis.c"));
+                }
+
+                #[cfg(not(feature = "with-stb"))]
+                {
+                    fn use_library(build: &mut cc::Build, pkg: &pkg_config::Library) {
+                        for (k, v) in pkg.defines {
+                            build.define(k, v);
+                        }
+                        build.includes(&pkg.include_paths);
+                        for path in &pkg.link_paths {
+                            println!("cargo:rustc-link-search={}", path);
+                        }
+                        for lib in &pkg.libs {
+                            add_lib(lib, cfg!(feature = "static"));
+                        }
+                    }
+
+                    build.define("SF3_SUPPORT", "SF3_XIPH_VORBIS");
+
+                    if let (Some(vorbis), Some(vorbisfile)) = (
+                        pkg_config::Config::new()
+                            .atleast_version("1.3.5")
+                            .probe("vorbis"),
+                        pkg_config::Config::new()
+                            .atleast_version("1.3.5")
+                            .probe("vorbisfile"),
+                    ) {
+                        use_library(&mut build, &vorbis);
+                        use_library(&mut build, &vorbisfile);
+                    } else {
+                        // use shipped libvorbis sources
+                        let src_dir = src_dir.join("libvorbis-1.3.5");
+
+                        build.include(src_dir.join("include"));
+                        build.include(src_dir.join("lib"));
+                        build.files(
+                            [
+                                "vorbisenc.c",
+                                "info.c",
+                                "analysis.c",
+                                "bitrate.c",
+                                "block.c",
+                                "codebook.c",
+                                "envelope.c",
+                                "floor0.c",
+                                "floor1.c",
+                                "lookup.c",
+                                "lpc.c",
+                                "lsp.c",
+                                "mapping0.c",
+                                "mdct.c",
+                                "psy.c",
+                                "registry.c",
+                                "res0.c",
+                                "sharedbook.c",
+                                "smallft.c",
+                                "vorbisfile.c",
+                                "window.c",
+                                "synthesis.c",
+                            ]
+                            .iter()
+                            .map(|src| src_dir.join("lib").join(src)),
+                        );
+                    }
+
+                    if let Some(ogg) = pkg_config::Config::new()
+                        .atleast_version("1.3.2")
+                        .probe("ogg")
+                    {
+                        use_library(&mut build, &ogg);
+                    } else {
+                        // use shipped libogg sources
+                        let src_dir = src_dir.join("libogg-1.3.2");
+
+                        build.include(src_dir.join("include"));
+                        build.files(
+                            ["bitwise.c", "framing.c"]
+                                .iter()
+                                .map(|src| src_dir.join("src").join(src)),
+                        );
+                    }
+                }
+            }
+
+            #[cfg(feature = "shared")]
+            build.shared_flag(true);
+
+            #[cfg(feature = "static")]
+            build.static_flag(true);
+
+            build.compile("fluidlite");
         }
 
         println!("cargo:rustc-link-search=native={}", lib_dir.display());
 
-        #[cfg(feature = "shared")]
-        println!("cargo:rustc-link-lib={}", lib_name);
-
-        #[cfg(not(feature = "shared"))]
-        println!("cargo:rustc-link-lib=static={}", lib_name);
+        add_lib(lib_name, !cfg!(not(feature = "shared")));
     }
 }
